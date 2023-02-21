caf37a46bceeecbd252fd8b62e6310f6232e4500
niraj <niraj.surati01@gmail.com>
2023-02-22 02:35:05 +0530

    22-02-2023 || resolves env variable issue

--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -1,19 +1,13 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-
+console.log(import.meta);
 const app = firebase.initializeApp({
-	// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	// authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	// projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	// storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	// messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	// appId: process.env.REACT_APP_FIREBASE_APP_ID,
-	apiKey: "AIzaSyB3l91Im4J5UhYAzr0YaPzPQEF4-cf327Y",
-	authDomain: "auth-development-88ead.firebaseapp.com",
-	projectId: "auth-development-88ead",
-	storageBucket: "auth-development-88ead.appspot.com",
-	messagingSenderId: "1034919669345",
-	appId: "1:1034919669345:web:4598711cc741c5b585c420",
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 });
 
 export const auth = app.auth();
